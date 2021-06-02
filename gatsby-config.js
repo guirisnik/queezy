66b287af32f94c836f83b54585ee24a536a836de
Guilherme Risnik <guilherme.risnik@gmail.com>
2021-06-02 15:06:21 -0300

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,21 @@
-module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+const path = require('path')
+
+const rootImportPlugin = {
+  resolve: 'gatsby-plugin-root-import',
+  options: {
+    src: path.join(__dirname, 'src'),
+    assets: path.join(__dirname, 'src/assets'),
+    components: path.join(__dirname, 'src/components'),
+    constants: path.join(__dirname, 'src/constants'),
+    hooks: path.join(__dirname, 'src/hooks'),
+    masks: path.join(__dirname, 'src/masks'),
+    pages: path.join(__dirname, 'src/pages'),
+    schemas: path.join(__dirname, 'src/schemas'),
+    screens: path.join(__dirname, 'src/screens'),
+    utils: path.join(__dirname, 'src/utils'),
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+}
+
+module.exports = {
+  plugins: [rootImportPlugin].filter(Boolean),
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyARwOq1uTnifjhTkTC5pHjXWhSu4YY8oBw',
+  authDomain: 'queezy-89549.firebaseapp.com',
+  projectId: 'queezy-89549',
+  storageBucket: 'queezy-89549.appspot.com',
+  databaseURL: 'https://queezy.firebaseio.com',
+  messagingSenderId: '599365589964',
+  appId: '1:599365589964:web:6fecb0d887389f39db5b69',
+  measurementId: 'G-56BL1X81WW',
+}
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase

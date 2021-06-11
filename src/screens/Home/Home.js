63a15ff5faf059796289f469107df41ce431e622
Guilherme Risnik <guilherme.risnik@gmail.com>
2021-06-11 12:52:53 -0300

    Remove weird JSON from Home

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import firebase from 'src/firebase'
 import { Layout } from 'components/Layout'
+import { Heading } from 'bumbag'
 
 const Home = () => (
   <Layout>
-    <pre>{JSON.stringify(firebase.auth().currentUser, null, 2)}</pre>
+    <Heading>Welcome, {firebase.auth().currentUser?.displayName}!</Heading>
   </Layout>
 )
 

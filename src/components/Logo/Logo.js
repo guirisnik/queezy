df56095d6e0702d393a7e4d3764882965286db98
Guilherme Risnik <guilherme.risnik@gmail.com>
2021-06-11 20:18:37 -0300

    Temporary fix to logo images

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,15 +4,19 @@ import Examination from 'assets/examination.svg'
 import Graduation from 'assets/graduation.svg'
 import Notebook from 'assets/notebook.svg'
 import Test from 'assets/test.svg'
+import 'utils/Array.extensions'
 
 const Logo = () => {
-  const Images = [Education, Examination, Graduation, Notebook, Test]
+  // TODO: Fix image shuffling
+  // const [randomImageSource] = [
+  //   Education,
+  //   Examination,
+  //   Graduation,
+  //   Notebook,
+  //   Test,
+  // ].shuffle()
 
-  const randomImageIndex = Math.floor(Math.random() * Images.length)
-
-  const RandomImage = Images[randomImageIndex]
-
-  return <img src={RandomImage} width='70%' />
+  return <img src={Test} width='70%' />
 }
 
 export { Logo }
---
+++ src/utils/Array.extensions.js
@@ -0,0 +1,21 @@
+import { identity } from './functional'
+
+Array.prototype.copy = function copy() {
+  return this.map(identity)
+}
+
+Array.prototype.shuffle = function shuffle() {
+  for (let index = this.length; index > 0; index--) {
+    const randomIndex = Math.floor(Math.random() * index)
+    this.swap(--index, randomIndex)
+  }
+
+  return this
+}
+
+Array.prototype.swap = function swap(indexA, indexB) {
+  const a = this[indexA]
+  this[indexA] = this[indexB]
+  this[indexB] = a
+  return new Promise((resolve, reject) => resolve(this))
+}
